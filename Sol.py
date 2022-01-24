2277f538671c068c48a031d03f9ba757bc92ceeb
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2022-01-24 18:54:18 +0530

    Create Sol.py

---
+++ Sol.py
@@ -0,0 +1,8 @@
+dict1 = {"Bread":20, "Cheese":30, "Tomato":20, "Onion":20, "Pickle":10}
+
+sum = 0
+
+for cost in dict1.values():
+    sum += cost
+    
+print("The total cost for preparing a Sandwhich is : Rs.",sum)
